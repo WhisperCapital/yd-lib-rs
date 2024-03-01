f268551dc69260796c0db76bd665068096e64974
linonetwo <linonetwo012@gmail.com>
2024-03-01 06:55:53 +0000

    refactor: simplify fn handle_record

--- crates/yd_client_sys/build_utils/handlers/config.rs
+++ crates/yd_client_sys/build_utils/handlers/config.rs
@@ -1,9 +1,13 @@
-use super::{handle_function_parameter::ParameterFlavor, handle_function_prototype::MethodFlavor};
+use super::{
+    handle_function_parameter::ParameterFlavor, handle_function_prototype::MethodFlavor,
+    handle_record::RecordFlavor,
+};
 
 #[derive(Clone)]
 pub struct HandlerConfigs {
     pub method_flavor: MethodFlavor,
     pub parameter_flavor: ParameterFlavor,
+    pub record_flavor: RecordFlavor,
     pub record_name: String,
 }
 
@@ -12,6 +16,7 @@ impl Default for HandlerConfigs {
         HandlerConfigs {
             method_flavor: MethodFlavor::None,
             parameter_flavor: ParameterFlavor::None,
+            record_flavor: RecordFlavor::None,
             record_name: "DefaultName".to_string(),
         }
     }
--- crates/yd_client_sys/build_utils/handlers/handle_record.rs
+++ crates/yd_client_sys/build_utils/handlers/handle_record.rs
@@ -6,6 +6,16 @@ use crate::build_utils::{
     handle_function_prototype::MethodFlavor, process_children, HandlerMap,
 };
 
+#[derive(Clone)]
+pub enum RecordFlavor {
+    /// Callback, need to generate a trait, stream, v-table...
+    SPI,
+    /// Generate safe wrapper around C++ API
+    API,
+    /// Log only
+    None,
+}
+
 pub fn handle_record(
     entity: &Entity,
     handlers: &HandlerMap,
@@ -15,30 +25,86 @@ pub fn handle_record(
         "// Record: {}",
         entity.get_display_name().unwrap_or_default()
     )];
-    // building safe rust binding for class
     let full_rust_struct_name = get_full_name_of_entity(&entity);
-    // TODO: currently only parse the SPI, do this for more structs like API later
     if full_rust_struct_name != "YDListener" {
         return lines;
     }
-    // handle spi callbacks
-    // first lines are trait
-    let full_trait_name = format!("{full_rust_struct_name}_trait",);
-    lines.push(format!(r#"pub trait {full_trait_name}: Send {{"#,));
-    // trait methods
+    let vtable_struct_name = format!("{full_rust_struct_name}VTable");
+    let full_trait_name = format!("{full_rust_struct_name}_trait");
+
+    lines.extend(handle_trait(
+        entity,
+        handlers,
+        configs,
+        &full_rust_struct_name,
+        &full_trait_name,
+    ));
+    lines.extend(handle_vtable(
+        entity,
+        handlers,
+        configs,
+        &full_rust_struct_name,
+        &vtable_struct_name,
+    ));
+    lines.extend(handle_spi_output_enum(
+        entity,
+        handlers,
+        configs,
+        &full_rust_struct_name,
+    ));
+    lines.extend(handle_output_enum_struct(entity, handlers, configs));
+    lines.extend(handle_static_table(
+        entity,
+        handlers,
+        configs,
+        &full_rust_struct_name,
+    ));
+    lines.extend(handle_c_fn(entity, handlers, configs));
+    lines.extend(handle_fat_spi(
+        entity,
+        handlers,
+        configs,
+        &full_rust_struct_name,
+        &vtable_struct_name,
+        &full_trait_name,
+    ));
+    lines.push(handle_spi_stream_code(
+        &full_rust_struct_name,
+        &format!("{full_rust_struct_name}Output"),
+    ));
+
+    lines
+}
+
+fn handle_trait(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+    full_rust_struct_name: &str,
+    full_trait_name: &str,
+) -> Vec<String> {
+    let mut lines = Vec::new();
+    lines.push(format!(r#"pub trait {full_trait_name}: Send {{"#));
     lines.extend(process_children(
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output trait style code
             method_flavor: MethodFlavor::Trait,
             ..configs.clone()
         },
     ));
     lines.push("}".to_string());
-    // end trait
-    // next are virtual tables
-    let vtable_struct_name = format!("{full_rust_struct_name}VTable");
+    lines
+}
+
+fn handle_vtable(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+    full_rust_struct_name: &str,
+    vtable_struct_name: &str,
+) -> Vec<String> {
+    let mut lines = Vec::new();
     lines.push(format!(
         r#"
 #[repr(C)]
@@ -49,14 +115,21 @@ struct {vtable_struct_name} {{"#
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output v-table struct style code
             method_flavor: MethodFlavor::Struct,
             ..configs.clone()
         },
     ));
     lines.push("}".to_string());
-    // end virtual tables
-    // next are spi output enum
+    lines
+}
+
+fn handle_spi_output_enum(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+    full_rust_struct_name: &str,
+) -> Vec<String> {
+    let mut lines = Vec::new();
     let full_spi_output_enum_name = format!("{full_rust_struct_name}Output");
     lines.push(format!(
         r#"
@@ -67,27 +140,39 @@ pub enum {full_spi_output_enum_name} {{"#
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output v-table struct style code
             method_flavor: MethodFlavor::OutputEnum,
             ..configs.clone()
         },
     ));
     lines.push("}".to_string());
-    // end spi output enum
-    // next are output enum struct
-    lines.extend(process_children(
+    lines
+}
+
+fn handle_output_enum_struct(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+) -> Vec<String> {
+    process_children(
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output v-table struct style code
             method_flavor: MethodFlavor::OutputEnumStruct,
             ..configs.clone()
         },
-    ));
-    // end output enum struct
-    // next are static table
+    )
+}
+
+fn handle_static_table(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+    full_rust_struct_name: &str,
+) -> Vec<String> {
+    let mut lines = Vec::new();
     let full_static_vtable_var_name =
-        Inflector::to_snake_case(&full_rust_struct_name).to_uppercase() + "_VTABLE";
+        Inflector::to_snake_case(full_rust_struct_name).to_uppercase() + "_VTABLE";
+    let vtable_struct_name = format!("{full_rust_struct_name}VTable");
     lines.push(format!(
         r#"
 static {full_static_vtable_var_name}: {vtable_struct_name} = {vtable_struct_name} {{"#
@@ -96,24 +181,109 @@ static {full_static_vtable_var_name}: {vtable_struct_name} = {vtable_struct_name
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output v-table struct style code
             method_flavor: MethodFlavor::StaticTable,
             ..configs.clone()
         },
     ));
     lines.push("}".to_string());
-    // end static table
-    // next are c fn
-    lines.extend(process_children(
+    lines
+}
+
+fn handle_c_fn(entity: &Entity, handlers: &HandlerMap, configs: &HandlerConfigs) -> Vec<String> {
+    process_children(
         entity,
         handlers,
         &mut HandlerConfigs {
-            // ask function handler to output v-table struct style code
             method_flavor: MethodFlavor::CFn,
             ..configs.clone()
         },
-    ));
-    // end c fn
+    )
+}
 
+fn handle_fat_spi(
+    entity: &Entity,
+    handlers: &HandlerMap,
+    configs: &HandlerConfigs,
+    full_rust_struct_name: &str,
+    vtable_struct_name: &str,
+    full_trait_name: &str,
+) -> Vec<String> {
+    let mut lines = Vec::new();
+    lines.push(format!(
+        r#"
+#[repr(C)]
+pub struct {full_rust_struct_name}Fat {{
+    vtable: *const {vtable_struct_name},
+    pub md_spi_ptr: *mut dyn {full_trait_name},
+}}
+"#
+    ));
     lines
+}
+
+fn handle_spi_stream_code(full_spi_name: &str, full_spi_output_enum_name: &str) -> String {
+    format!(
+        r#"
+    use futures::stream::Stream;
+    use std::{{
+        pin::Pin,
+        sync::{{Arc, Mutex}},
+        task::Waker,
+    }};
+
+    struct {full_spi_name}Inner {{
+        buf: std::collections::VecDeque<{full_spi_output_enum_name}>,
+        waker: Option<Waker>,
+    }}
+
+    impl {full_spi_name}Inner {{
+        fn push(&mut self, msg: {full_spi_output_enum_name}) {{
+            self.buf.push_back(msg);
+            if let Some(ref waker) = &self.waker {{
+                waker.clone().wake()
+            }}
+        }}
+    }}
+
+    pub struct {full_spi_name}Stream {{
+        inner: Arc<Mutex<{full_spi_name}Inner>>,
+    }}
+
+    impl Stream for {full_spi_name}Stream {{
+        type Item = {full_spi_output_enum_name};
+
+        fn poll_next(
+            self: Pin<&mut Self>,
+            cx: &mut futures::task::Context<'_>,
+        ) -> futures::task::Poll<Option<Self::Item>> {{
+            use futures::task::Poll;
+            let mut inner = self.inner.lock().unwrap();
+            if let Some(i) = inner.buf.pop_front() {{
+                Poll::Ready(Some(i))
+            }} else {{
+                inner.waker = Some(cx.waker().clone());
+                Poll::Pending
+            }}
+        }}
+
+        fn size_hint(&self) -> (usize, Option<usize>) {{
+            (0, None)
+        }}
+    }}
+
+    pub fn create_spi() -> (Box<{full_spi_name}Stream>, *mut {full_spi_name}Stream) {{
+        let i = {full_spi_name}Inner {{
+            buf: std::collections::VecDeque::new(),
+            waker: None,
+        }};
+        let xspi = {full_spi_name}Stream {{
+            inner: Arc::new(Mutex::new(i)),
+        }};
+        let myspi = Box::new(xspi);
+        let pp = Box::into_raw(myspi);
+        let pp2 = pp.clone();
+        (unsafe {{ Box::from_raw(pp2) }}, pp)
+    }}
+    "#,
+    )
 }
